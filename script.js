9064abb9360ab2bc0b146aa0a05c9bac12ed6036
tannavrushti <tannavrushti76@gmail.com>
2026-01-28 21:13:05 +0530

    Add new javascript

---
+++ script.js
@@ -0,0 +1,64 @@
+//console demo
+console.log("Hello, World! This is a demo script.");
+
+//varible demo
+let name = "tanna";
+const age = 23;
+console.log("Name:", name);
+console.log("Age:", age);
+
+//data type demo
+let a = 10; //number
+let b = "Hello"; //string
+let c = true; //boolean
+let d; //undefined
+console.log(a, b, c, d);
+
+//if -else demo
+let marks = 70;
+if (marks >= 35) {
+    console.log("pass");
+} else {
+    console.log("fail");
+}
+
+//arithmatic operation demo
+let num1 = 10;
+let num2 = 5;
+console.log(num1 + num2);
+
+//assignment operation demo
+let x = 20;
+x = x + 2;
+console.log(x);
+
+//loop demo
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+//array demo
+let cars = ["BMW", "Audi", "Toyota"];
+console.log(cars[0]);
+console.log(cars.length);
+
+//function demo
+function demo() {
+    console.log("Welcome to the JS demo!");
+
+}
+demo();
+
+//object demo
+let student = {
+    name: "Twins",
+    age: 22,
+    grade: "A",
+    course: "MCA"
+};
+console.log(student.course);
+
+
+
+
+
