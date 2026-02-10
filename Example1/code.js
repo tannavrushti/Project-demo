7519bc076fa1dd07053d09940a303034e3cede4c
tannavrushti <tannavrushti76@gmail.com>
2026-02-10 15:24:43 +0530

    Add some new features

--- Example1/code.js
+++ Example1/code.js
@@ -4,3 +4,9 @@ function handleMyClick() {
     alert("The button works!");
     // You can add more complex logic here
 }
+function addToCart() {
+    // Your existing logic goes here
+    console.log("Button was clicked!");
+    alert("Add to Cart Successfully!");
+    // You can add more complex logic here
+}
